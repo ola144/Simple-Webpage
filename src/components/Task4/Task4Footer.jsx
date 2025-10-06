89156021b8019622d5b6c668f37152fb5d72b3d7
Ola <oladimejiagbaje@gmail.com>
2025-10-06 11:08:17 +0100

    footer-adjust

--- src/components/Task4/Task4Footer.jsx
+++ src/components/Task4/Task4Footer.jsx
@@ -3,7 +3,7 @@ import "./Task4Footer.css";
 const Task4Footer = () => {
   return (
     <footer className="site-footer">
-      <div className="container">
+      <div className="">
         <p>
           © {new Date().getFullYear()} Oladimeji Agbaje — Built with Vite +
           React
